025b050af0f004e39065340d113ea6747b3642f7
JoshRinaberger <joshrinaberger@gmail.com>
2023-04-03 21:50:33 -0500

    Created a simple grey perlin noise and a color based perlin noise effect.

--- Perlin Noise/sketch.js
+++ Perlin Noise/sketch.js
@@ -1,7 +1,50 @@
 function setup() {
   createCanvas(800, 800);
+  colorMode(HSB, 360, 100, 100, 100);
+  noStroke();
+  //simplePerlinNoise();
+  coloredPerlinNoise();
 }
 
+/*
 function draw() {
   background(220);
+}*/
+
+function simplePerlinNoise() {
+  let resolution = 0.02;
+
+  for (i = 0; i < width; i++) {
+    for (j = 0; j < height; j++) {
+      let n = noise(i * resolution, j * resolution);
+      let alph = 40;
+      let brt = map(n, 0, 1, 0, 100);
+      fill(0, 0, brt, alph);
+
+      rect(i, j, 3);
+    }
+  }
+}
+
+function coloredPerlinNoise() {
+  let resolution = 0.02;
+
+  for (i = 0; i < width; i++) {
+    for (j = 0; j < height; j++) {
+      let n = noise(i * resolution, j * resolution) * 10;
+
+      let huey;
+
+      if (n < 2){huey = 80}
+      else if (n < 4){huey = 150}
+      else if (n < 4){huey = 210}
+      else if (n < 4){huey = 280}
+      else {huey = 360}
+
+      let alph = 40;
+      fill(huey, 90, 90, alph);
+
+      rect(i, j, 3);
+    }
+  }
 }
