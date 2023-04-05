d94cebd026391850696ec2892a464d1f09c2827b
JoshRinaberger <joshrinaberger@gmail.com>
2023-04-04 22:11:48 -0500

    Added leaves to the recursive trees.

--- Trees/sketch.js
+++ Trees/sketch.js
@@ -1,3 +1,5 @@
+var addLeaves = true;
+
 function setup() {
   createCanvas(800, 800);
   angleMode(DEGREES);
@@ -32,6 +34,35 @@ function drawBranch(length) {
 
     rotate(random(-50, -60))
     drawBranch(length * random(.7, .9));
+  } else if (addLeaves) {
+
+    // green leaves
+    /*var r = 80 + random(-20, -20);
+    var g = 120 + random(-20, 20);
+    var b = 40 + random (-20, 20);*/
+
+    // pink leaves
+    var r = 220 + random(-20, -20);
+    var g = 120 + random(-20, 20);
+    var b = 170 + random (-20, 20);
+    fill(r, g, b);
+    noStroke();
+    
+    beginShape();
+    for (var i = 45; i < 135; i++) {
+      var rad = 15;
+      var x = rad * cos(i);
+      var y = rad * sin(i);
+      vertex(x, y);
+    }
+
+    for (var i = 135; i > 45; i--) {
+      var rad = 15;
+      var x = rad * cos(i);
+      var y = rad * sin(-i) + 20;
+      vertex(x, y);
+    }
+    endShape(CLOSE);
   }
   pop();
 }
